ad7240549d113d93dde7fa5e331be82dcac8df33
Marko Bauhardt <marko.bauhardt@mailbox.org>
2019-06-30 17:04:05 +0200

    added id generator

--- persistence/persistence.go
+++ persistence/persistence.go
@@ -1,15 +1,37 @@
 package persistence
 
-import "os"
+import (
+	"errors"
+	"os"
+	"strconv"
+)
 
 type Environment struct {
-  DbPath string;
+	DbPath string
 }
 
 func Env() (*Environment, error) {
-  hdir, e := os.UserHomeDir()
-  if e != nil {
-    return nil, e
-  }
-  return &Environment { DbPath: hdir + "/.local/share/moneyflow/db" }, nil
+	hdir, e := os.UserHomeDir()
+	if e != nil {
+		return nil, e
+	}
+	return &Environment{DbPath: hdir + "/.local/share/moneyflow/db"}, nil
+}
+
+func GenerateDocumentId(env *Environment) (string, error) {
+	for i := 0; i < 10; i++ {
+		path := env.DbPath + "/" + strconv.Itoa(i)
+		if !exists(path) {
+			return strconv.Itoa(i), nil
+		}
+	}
+	return "", errors.New("unable to generate")
+}
+
+// exists returns whether the given file or directory exists
+func exists(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
--- persistence/persistence_test.go
+++ persistence/persistence_test.go
@@ -1,13 +1,14 @@
 package persistence
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 )
 
 func TestEnv(t *testing.T) {
-	hdir,_ := os.UserHomeDir()
-	
+	hdir, _ := os.UserHomeDir()
 	tests := []struct {
 		name     string
 		expected Environment
@@ -23,4 +24,36 @@ func TestEnv(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestIdGenerator(t *testing.T) {
+	dir, _ := ioutil.TempDir("", "example")
+	defer os.RemoveAll(dir) // clea
+
+	tests := []struct {
+		name  string
+		env   Environment
+		files []string
+		id    string
+	}{
+		{"no files", Environment{DbPath: dir + "/a"}, []string{}, "0"},
+		{"one file 0", Environment{DbPath: dir + "/b"}, []string{"0"}, "1"},
+		{"two files 0,1", Environment{DbPath: dir + "/c"}, []string{"0", "1"}, "2"},
+		{"one file 1", Environment{DbPath: dir + "/d"}, []string{"1"}, "0"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, f := range tt.files {
+				p := tt.env.DbPath + "/" + f
+				e := os.MkdirAll(p, os.ModePerm)
+				fmt.Println(e)
+			}
+			docid, _ := GenerateDocumentId(&tt.env)
+			if docid != tt.id {
+				t.Errorf("TestIdGenerator() == %q, want %q", docid, tt.id)
+			}
+		})
+	}
+
 }
