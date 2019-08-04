dea6cb3be7ba31443aca5a81a842c098ff7d50a9
Marko Bauhardt <marko.bauhardt@mailbox.org>
2019-08-04 17:22:00 +0200

    implement new database doc

--- moneyflow.go
+++ moneyflow.go
@@ -1,17 +1,15 @@
 package main
 
-import "fmt"
-import "regexp"
-import "os"
-import "strings"
+import (
+	//"fmt"
+	"github.com/mbauhardt/moneyflow/persistence"
+)
 
 func main() {
-    argsWithoutProg := strings.Join(os.Args[1:], " ")
-	tagRegex, _ := regexp.Compile("\\+[a-zA-Z]+")
-	moneyRegex, _ := regexp.Compile("money:[0-9\\.]+")
-	fmt.Println(tagRegex.FindAllString(argsWithoutProg, -1))
-	fmt.Println(moneyRegex.FindAllString(argsWithoutProg, -1))
-	fmt.Println(tagRegex.ReplaceAllString(argsWithoutProg, " "))
-	fmt.Println(moneyRegex.ReplaceAllString(argsWithoutProg, " "))
-	fmt.Println(moneyRegex.ReplaceAllString(tagRegex.ReplaceAllString(argsWithoutProg, " "), " "))
+
+	env, err := persistence.Env()
+	if err != nil {
+		panic(err)
+	}
+	persistence.NewDatabaseDocument(env)
 }
--- persistence/persistence.go
+++ persistence/persistence.go
@@ -2,12 +2,20 @@ package persistence
 
 import (
 	"errors"
+	"github.com/mbauhardt/moneyflow/entities"
 	"os"
 	"strconv"
 )
 
 type Environment struct {
 	DbPath string
+}
+
+type DatabaseDocument struct {
+	id    string
+	money *entities.Money
+	desc  *string
+	tags  *[]entities.Tag
 }
 
 func Env() (*Environment, error) {
@@ -19,16 +27,39 @@ func Env() (*Environment, error) {
 }
 
 func GenerateDocumentId(env *Environment) (string, error) {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 10000000; i++ {
 		path := env.DbPath + "/" + strconv.Itoa(i)
 		if !exists(path) {
 			return strconv.Itoa(i), nil
 		}
 	}
-	return "", errors.New("unable to generate")
+	return "", errors.New("unable to generate database id")
 }
 
-// exists returns whether the given file or directory exists
+func NewDatabaseDocument(env *Environment) (*DatabaseDocument, error) {
+	id, err1 := GenerateDocumentId(env)
+	if err1 != nil {
+		return nil, err1
+	}
+	err2 := os.MkdirAll(env.DbPath+"/"+id, os.ModePerm)
+	if err2 != nil {
+		return nil, err2
+	}
+	_, err3 := os.Create(env.DbPath + "/" + id + "/money")
+	if err3 != nil {
+		return nil, err3
+	}
+	_, err4 := os.Create(env.DbPath + "/" + id + "/description")
+	if err4 != nil {
+		return nil, err4
+	}
+	_, err5 := os.Create(env.DbPath + "/" + id + "/tags")
+	if err5 != nil {
+		return nil, err5
+	}
+	return &DatabaseDocument{id: id}, nil
+}
+
 func exists(path string) bool {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return false
